32aa22f06fb56f3eefb7abb20f6895da4883719f
victorlora <vl248684@muhlenberg.edu>
2015-11-27 03:30:32 -0500

    Began working on backend to gather car info

--- Voltage/Voltage/CarInfoViewController.swift
+++ Voltage/Voltage/CarInfoViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class CarInfoViewController: UIViewController {
+class CarInfoViewController: UIViewController, UITableViewDelegate {
     
     private let APIKey = "6m8ettta5byepu43rkhsc79j"
+    var carInfo = [String]()
+    
+    @IBOutlet var carInfoTable: UITableView!
     
     var id = ""
 
@@ -19,12 +22,41 @@ class CarInfoViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         getStyleId()
+        getCarInfo()
+        resetUserDefaults()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    /* numberOfSectionsInTableView()
+    * @returns
+    *      Number of columns in tableView
+    */
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    /* tableView()
+    * @returns
+    *      Number of items in current array for tableview rows
+    */
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return carInfo.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = String(carInfo [indexPath.row])
+        
+        return cell
+    }
+    
     
 
     /* getCarInfo()
@@ -54,7 +86,6 @@ class CarInfoViewController: UIViewController {
                                 for year in years {
                                     if let carYear = year["year"] as? Int {
                                         if (carYear == Int(userYear)) {
-                                            print(carYear)
                                             if let styles = year["styles"] as? [[String: AnyObject]] {
                                                 for style in styles {
                                                     if let carStyle = style["name"] as? String {
@@ -81,14 +112,47 @@ class CarInfoViewController: UIViewController {
     }
     
     func getCarInfo() {
+    
+        // Setup the session to make REST GET call.  Notice the URL is https NOT http!!
         
+        let url = NSURL(string: "https://api.edmunds.com/api/vehicle/v2/styles/17826?view=full&fmt=json&api_key=6m8ettta5byepu43rkhsc79j")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            
+            if let urlContent = data {
+                do {
+                    // TO DO: Parse JSON
+                    let jsonResult = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.AllowFragments) as! NSDictionary
+                    if let engine = jsonResult["engine"] as? [[String: AnyObject]] {
+                        print("we made it")
+                        print(String(engine))
+                    } else {
+                        print("error")
+                    }
+
+                    
+                } catch {
+                    
+                    print("JSON serialization failed")
+                    
+                }
+                
+                
+            }
+            
+            
+        }
+
+        task.resume()
+        
+        carInfoTable.reloadData()
     }
     
 
-//    func resetUserDefaults() {
-//        NSUserDefaults.standardUserDefaults().setObject(nil, forKey: "make")
-//        NSUserDefaults.standardUserDefaults().setObject(nil, forKey: "model")
-//        NSUserDefaults.standardUserDefaults().setObject(nil, forKey: "year")
-//    }
+    func resetUserDefaults() {
+        NSUserDefaults.standardUserDefaults().setObject(nil, forKey: "make")
+        NSUserDefaults.standardUserDefaults().setObject(nil, forKey: "model")
+        NSUserDefaults.standardUserDefaults().setObject(nil, forKey: "year")
+    }
 
 }
