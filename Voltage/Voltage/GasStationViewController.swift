e7e40c3f0b2978042abb7ff6c6ef9f933b21a056
1volt3 <tigerjosh13@yahoo.com>
2015-12-06 12:19:36 -0500

    Added GasStationViewController

---
+++ Voltage/Voltage/GasStationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GasStationViewController.swift
+//  Voltage
+//
+//  Created by Josh Rosenzweig on 12/6/15.
+//  Copyright © 2015 EV-APP. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import Foundation
+
+class GasStationViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        let refreshAlert = UIAlertController(title: "Memory Warning", message: "All data cannot be saved.", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        refreshAlert.addAction(UIAlertAction(title: "Ok", style: .Default, handler: { (action: UIAlertAction!) in
+            
+        }))
+    }
+    
+    
+}
