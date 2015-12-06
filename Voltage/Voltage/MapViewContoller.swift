ba788c11df5de06b52aabd024c8d8178f1b26f6d
victorlora <vl248684@muhlenberg.edu>
2015-12-05 21:23:03 -0500

    Enabled features in Map View

--- Voltage/Voltage/MapViewContoller.swift
+++ Voltage/Voltage/MapViewContoller.swift
@@ -13,8 +13,10 @@ import Foundation
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet var speedLabel: UILabel!
     
     var locationManager = CLLocationManager()
+    var startingLocation = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,9 +53,18 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
         let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
         
         let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+       
+        // center map at this region
+        self.mapView.setRegion(region, animated: true)
         
-        self.mapView.setRegion(region, animated: false)
+        self.mapView.showsUserLocation = true;
+        self.mapView.showsScale = true;
+        self.mapView.showsPointsOfInterest = true;
+        self.mapView.showsTraffic = true;
         
+        // print user speed
+        speedLabel.text = String(format: "%.2f", userLocation.speed * 2.23694)
+
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
